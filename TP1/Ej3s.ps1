0dad5b4325c28f995f93d2493674b79d2cf20a43
DrCoffee84 <daniel.boullon@yahoo.com>
2016-04-06 13:36:06 -0300

    no esta terminado, pero ya carga bien la matriz
    Pero para que se vaya viendo algo

--- TP1/Ej3s.ps1
+++ TP1/Ej3s.ps1
@@ -1,70 +1,99 @@
-﻿<#
+<#
 .SYNOPSIS
 Resuelve un sistema de ecuaciones de n variables y n incógnitas.
 .DESCRIPTION
 Recive un txt con los datos para hacer resolver el sistema de ecuaciones y lo guarda en otro txt de salida.
 .PARAMETER path
 El directorio del archivo en el que se requiere para sacar los coeficioneste de los datos
- ( por defecto en la raiz del script con el nombre gauss.txt)
-
+( por defecto en la raiz del script con el nombre gauss.txt)
 .EXAMPLE
-
-
-
 #>
-
 <#
 Nombre del Script: Ej3.ps1
 Trabajo Practico nro 1
 Programacion de scripts basicos en Powershell
 Ejercicio 03
-
 Integrantes del grupo:
 Greve Iván - 38617763
 Boullon Daniel Ernesto - 36385582
 Silvero Heber Ezequiel - 36404597
 Valenzuela Juan Santiago - 38624490
 Nicolás Satragno - 38527273
-
-
 Entrega: Primera Entrega
-
-
 #>
 
-
 Param(
-  #Este  va a recibir la entrada por pipe si hubiera. Si no, queda en "blanco"
-    [Parameter(Position=1, Mandatory = $false)][ValidateNotNullOrEmpty()][String] $path = "gauss.txt”    
+[Parameter(Position=1, Mandatory = $false)][ValidateNotNullOrEmpty()][String] $path = "gauss.txt”
 )
 
 $array = [System.Collections.ArrayList]@()
 
 $contenido = Get-Content $path
 
-$i = 0
-
+$i = $true
 
 foreach($obj in $contenido)
 {
-    if($i -eq 0)
-    {
-        $N = $obj
-    }
+    if($i)
+        {
+            [int]$N = $obj 
+            $i = $false;
+        }
     else
-    {
-        $linea = $obj -split ""
-        $array.Add($linea)
-        #Write-Host $linea
-  
-    }
-    $i++
+        {
+            $linea = $obj -split " "
+            [int]$array.Add($linea)      
+        }
 }
 
-Write-Host " "
-write-host $array[0][1] 
-#La matriz se llena pero desde la segunda fila nose porque(? si quieren jueguen con las posiciones y van a ver (Creo que guarda el salto de linea),
-#Cuando ejecuta nose porque motivo me muestra "0 1 2" , creo que es problema del metodo Add()
+write-host "N:"$N;
+Write-Host "Matriz: "
+write-host $array[0][0]
+write-host $array[0][1]
+write-host $array[0][2]
+write-host $array[1][0]
+write-host $array[1][1]
+write-host $array[1][2]
+write-host $array[2][0]
+write-host $array[2][1]
+write-host $array[2][2]
+write-host " " 
 
-Write-Host " "
-write-host "N:"$N
+for($j=0;$j-lt$N;$j++)
+{
+    write $j
+
+    $maxEl=[math]::abs($array[$j][$j]);
+    $maxRow=$j
+    for($k=$j+1;$k-lt$N;$k++) {
+        
+        if( [math]::abs($array[$k][$j])-gt$maxEl ){
+            $maxEl=$array[$k][$j];
+            $maxRow=$j
+
+        }    
+    }
+    
+
+    for($k=$j;$k-lt$N+1;$k++){
+                   
+        [int]$temp=$array[$maxRow][$k];
+        $array[$maxRow][$k]=$array[$j][$k];
+        $array[$j][$k]=$temp;
+   
+    }
+    
+    for($k=$j+1;$k-lt$N;$k++){
+    
+        $c= -$array[$k][$j]/$array[$j][$j];
+        for($l=$j;$l-lt$N+1;$l++){
+            if($j-eq$l){
+                $array[$k][$l]=0
+            }else{
+                $array[$k][$l]+=$c*$array[$j][$l]
+            }
+        }
+    }
+}
+
+write-host $array
